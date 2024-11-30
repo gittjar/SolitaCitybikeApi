cf3067dd4481ba3e928d1fa256b1148c8dd44858
gittjar <jarno.keranen@gmail.com>
2024-11-30 22:04:38 +0200

    app.py testing

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
-from routes.stations import stations_bp
 
 app = Flask(__name__)
-app.register_blueprint(stations_bp)
 
 @app.route('/')
-def hello_world():
-    return "This is API root"
+def hello():
+    return "Hello, World!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
