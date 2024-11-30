1d9ab3766e7e613f3b1c1d14525ba40914f285c7
gittjar <jarno.keranen@gmail.com>
2024-11-30 22:21:14 +0200

    update app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from routes.stations import stations_bp
 
 app = Flask(__name__)
+app.register_blueprint(stations_bp)
 
 @app.route('/')
-def hello():
+def hello_world():
     return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
