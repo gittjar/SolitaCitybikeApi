8e20662a26986bb4df147892e0a5ecb9c1deaaa9
gittjar <jarno.keranen@gmail.com>
2024-11-30 21:58:33 +0200

    app.py removed index

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 from routes.stations import stations_bp
-import datetime
 
 app = Flask(__name__)
 app.register_blueprint(stations_bp)
 
 @app.route('/')
-def index():
-    current_date = datetime.datetime.now().strftime("%d.%m.%Y")
-    current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    return render_template('index.html', current_date=current_date, current_time=current_time)
+def hello_world():
+    return "This is API root"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
