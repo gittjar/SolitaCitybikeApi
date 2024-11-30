f040cbc14495665f2528b6ee57986d99f5c11bff
gittjar <jarno.keranen@gmail.com>
2024-11-30 17:24:52 +0200

    requirements.txt

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def index():
     return render_template('index.html', current_date=current_date, current_time=current_time)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
