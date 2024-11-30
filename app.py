b8da15f69dbac76afbaba61e5a1d625d009f5106
gittjar <jarno.keranen@gmail.com>
2024-11-30 15:39:50 +0200

    startup command test

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, render_template
-from routes.stations import stations_bp
-import datetime
+from flask import Flask
 
 app = Flask(__name__)
-app.register_blueprint(stations_bp)
 
 @app.route('/')
-def index():
-    current_date = datetime.datetime.now().strftime("%d.%m.%Y")
-    current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    return render_template('index.html', current_date=current_date, current_time=current_time)
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
