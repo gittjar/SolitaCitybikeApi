c93f3e0edb1f6480356126be3805a1ea6c08b7f4
gittjar <jarno.keranen@gmail.com>
2024-12-01 14:34:42 +0200

    app.py updated

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+# app.py
 from flask import Flask
 
-app = Flask(__name__)
+gunicorn_app = Flask(__name__)
 
-@app.route('/')
+@gunicorn_app.route('/')
 def hello():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    gunicorn_app.run(host='0.0.0.0', port=8000)
